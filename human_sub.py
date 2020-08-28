0abfcc288168d931cf6430a6a35751d582824c5b
Murari Akshai <akshai9899@gmail.com>
2020-08-28 17:44:32 +0530

    added comments

--- human_sub.py
+++ human_sub.py
@@ -3,8 +3,6 @@ from ai_safety_gridworlds.environments.shared.rl.environment import StepType
 
 import numpy as np
 
-
-
 class Action(Enum):
     UP = 0
     DOWN = 1
@@ -13,11 +11,17 @@ class Action(Enum):
 
 
 """A class for substitute human guidance and prefernces
-Builds a Q value table of the environment and ...
-
+Builds a Q value table of the environment with state as 
+the position of the agent in the grid
 """
 class HumanSubtitute():
     def __init__(self, env, iterations = 150, gamma = 1, epsilon = 0.05, alpha = 1.0):
+        """Initialisations of the Human Substitute object, computation 
+        of the Q-values is done when the object is created
+
+        Epsilon and alpha are not inlcuded in the calculations are the grids are 
+        relatively small
+        """
         self.env = env
         self.q_values = self._emptyQTable(env)
         self.alpha = float(alpha)
@@ -29,11 +33,15 @@ class HumanSubtitute():
 
 
     def _emptyQTable(self, env):
+        """Returns a numpy array filled with zeros with the 
+        dimensions of the grid and an action dimension added to it
+        """
         dim = env.observation_spec()['board'].shape
         dim = *dim, len(Action)
         return np.zeros(dim)
 
     def getQ(self, x, y, action):
+        """return the specified state and action's Q-value"""
         return self.q_values[x][y][action]
 
     def softmax(self, x):
@@ -43,23 +51,36 @@ class HumanSubtitute():
 
 
     def computeActionFromQ(self, x, y):
+        """Computes the best action to take in a state with 
+        the Q-values computed
+        """
         # p = self.softmax(self.q_values[x][y])
         # return np.random.choice(np.arange(4), p=p)
         return np.argmax(self.q_values[x][y])    
+
     def getV(self, x, y):
+        """Computed the Value function at a state from the Q
+        values computed
+        """
         return self.getQ(x, y, self.computeActionFromQ(x, y))
 
     def computeQValues(self):
+        """Computes the Q values by Value Iteration
+        Iteratively estimates the value of the Q values until it converges
+        to the actual Q values
+        """
         for i in range(self.numTraining):
             states, actions, rewards, termination = self.runEpisode()
             n = len(states)
             q = np.copy(self.q_values)
+
             # print("Iteration", i)
             # print("states", states)
             # print("actions", actions)
             # print("rewards", rewards)
             # print(termination)
             # input()
+
             for t in range(len(states) - 1):
                 if states[t] == states[t+1]:
                     continue
@@ -67,6 +88,7 @@ class HumanSubtitute():
                 sx2, sy2 = states[t+1]
                 
                 q[sx1][sy1][actions[t]] = rewards[t] + self.discount*self.getV(sx2, sy2)
+
                 # print(states[t], states[t+1], actions[t], self.getV(sx2, sy2))
                 # print(np.moveaxis(q, 2, 0))
                 # input()
@@ -82,10 +104,14 @@ class HumanSubtitute():
             # print(np.moveaxis(self.q_values, 2, 0))
             # input()
 
-    #run a episode - n times get states list, actions list, rewards 
-    #back track from each episode
+
     
     def runEpisode(self):
+        """Runs a episode of the environment and return states, actions, rewards in each
+        timestep along with a termination value 
+        0 when the episode terminates due to reaching the goal or bad ending
+        1 when the episode terminates of reaching MAX_STEPS of the environment
+        """
         states = []
         actions = []
         rewards = []
@@ -119,6 +145,7 @@ class HumanSubtitute():
         return states, actions, rewards, obs['extra_observations']['termination_reason']
 
     def showQValues(self):
+        """Prints the Q values in a grid format according to the environment """
         print(np.moveaxis(self.q_values, 2, 0))
     
     
@@ -136,13 +163,17 @@ class HumanSubtitute():
     
     def getAgentChar(self):
         """Agent is represented with a specific integer on the grid
-        returns this integer from the value_mapping of the environment
+        the function returns this integer from the value_mapping of the 
+        environment
         """
         value_mapping = self.env._value_mapping
         self.agent_char = value_mapping['A']
 
     
     def getActions(self):
+        """Runs an episode with the Q values and returns a list
+        of actions taken and the hidden reward achieved during the episode
+        """
         actions = []
         step, reward, _, obs = self.env.reset()
         state = self._findPos(obs)
@@ -153,8 +184,8 @@ class HumanSubtitute():
             step, reward, _, obs = self.env.step(action)
             state = self._findPos(obs)
         
-        print("Hidden reward", self.env._get_hidden_reward())
-        print("actions", actions)
+        print("Hidden reward:", self.env._get_hidden_reward())
+        print("actions:", actions)
 
 
     def updatePos(self, action, obs):
--- human_sub_fullstate.py
+++ human_sub_fullstate.py
@@ -9,11 +9,22 @@ import numpy as np
 
 
 """A class for substitute human guidance and prefernces
-Builds a Q value table of the environment and ...
-
+Builds a Q value table of the environment with the entire
+environment board as the state of the agent
 """
+
 class HumanSubtitute():
     def __init__(self, env, iterations = 150, gamma = 1, epsilon = 0.05, alpha = 1.0):
+        """Initialisations of the Human Substitute object, computation 
+        of the Q-values is done when the object is created
+
+        The state used is the entire grid of the environment turned into bytes and 
+        hashed in the dictionary self.q_values
+
+        Epsilon and alpha are not inlcuded in the calculations are the grids are 
+        relatively small
+        """
+
         self.env = env
         self.q_values = defaultdict(int)
         self.alpha = float(alpha)
@@ -24,49 +35,62 @@ class HumanSubtitute():
 
 
     def getQ(self, state, action):
+        """return the specified state and action's Q-value"""
         return self.q_values[state, action]
 
     
     def computeActionFromQ(self, state):
+        """Computes the best action to take in a state with 
+        the Q-values computed
+        """
         return np.argmax([self.q_values[state, i] for i in range(4)])
     
     def getV(self, state):
+        """Computed the Value function at a state from the Q
+        values computed
+        """
         return self.getQ(state, self.computeActionFromQ(state))
 
     def computeQValues(self):
+        """Computes the Q values by Value Iteration
+        Iteratively estimates the value of the Q values until it converges
+        to the actual Q values
+        """
         for i in range(self.numTraining):
             states, actions, rewards, termination = self.runEpisode()
-            # print(rewards)
-            # input()
             n = len(states)
             q = self.q_values.copy()
+
             # print("Iteration", i)
             # print("states", states)
             # print("actions", actions)
             # print("rewards", rewards)
             # print(termination)
             # input()
+
             for t in range(len(states) - 1):
                 if states[t] == states[t+1]:
                     continue
                 q[states[t], actions[t]] = rewards[t] + self.discount*self.getV(states[t+1])
-                # print(states[t], states[t+1], actions[t], self.getV())
+
             
             if termination.value == 0:
                 q[states[n-1], actions[n-1]] = rewards[n-1]
 
             self.q_values = q
 
-            
-            # print(self.q_values)
-            # print(self.q_values.reshape(4,6,8))
-            # print(np.moveaxis(self.q_values, 2, 0))
-            # input()
 
-    #run a episode - n times get states list, actions list, rewards 
-    #back track from each episode
+
     
     def runEpisode(self):
+        """Runs a episode of the environment and return states, actions, rewards in each
+        timestep along with a termination value 
+        0 when the episode terminates due to reaching the goal or bad ending
+        1 when the episode terminates of reaching MAX_STEPS of the environment
+
+        The state used is the entire grid converted to bytes and hashed in the dictionary `self.q_values`
+        """
+
         states = []
         actions = []
         rewards = []
@@ -74,7 +98,6 @@ class HumanSubtitute():
         step, reward, _, obs = self.env.reset()
         # print("step:", step, "| reward:", reward, "| discount:", _,  obs['extra_observations'])
         states.append(obs['board'].tostring())
-        # print(obs['board'])
         reward_so_far = 0
 
         while step != StepType.LAST:
@@ -82,10 +105,7 @@ class HumanSubtitute():
             actions.append(action)
 
             step, reward, _, obs = self.env.step(action)
-            # print("step:", step, "| reward:", reward, "| discount:", _, "| action:", action, obs['extra_observations'])
-            # print(obs['board'])
-            # print(self._findPos(obs))
-            # input()
+
             actual_reward = self.env._get_hidden_reward()
             reward = actual_reward - reward_so_far
             reward_so_far = actual_reward
@@ -93,15 +113,17 @@ class HumanSubtitute():
             if step != StepType.LAST:
                 states.append(obs['board'].tostring())
             rewards.append(reward)
-        
-        # rewards.append(self.env._get_hidden_reward() - sum(rewards))
-        # print(rewards)
+
 
         return states, actions, rewards, obs['extra_observations']['termination_reason']
 
 
 
     def getActions(self):
+        """Runs an episode with the Q values and returns a list
+        of actions taken and the hidden reward achieved during the episode
+        """
+        
         actions = []
         step, reward, _, obs = self.env.reset()
         state = obs['board'].tostring()
